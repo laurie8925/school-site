b5d3643e8e5ad74ff9602305217eaa1e14924ff1
Rathan <rathan_30@hotmail.com>
2024-03-13 19:13:06 -0700

    made a news page.php, no content added to file

--- functions.php
+++ functions.php
@@ -176,3 +176,9 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+function fwd_excerpt_more( $more) {
+	$more = '...<a href="'. esc_url(get_permalink()) .'">Continue Reading</a>';
+	return $more;
+}
+add_filter( 'excerpt_more', 'fwd_excerpt_more');
+
